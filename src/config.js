189881cfaeb07c20ad70ecd6882697b11b37fa1f
Pu Du <rocketsboy@gmail.com>
2020-05-12 12:20:18 -0500

    setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-east-2',
+    BUCKET: 'keepernote',
+  },
+  apiGateway: {
+    REGION: 'us-east-2',
+    URL: 'https://98a8t1olqh.execute-api.us-east-2.amazonaws.com/prod',
+  },
+  cognito: {
+    REGION: 'us-east-2',
+    USER_POOL_ID: 'us-east-2_8ssnNpHBX',
+    APP_CLIENT_ID: '91eod2k3cq7fkh03stbpqhqo7',
+    IDENTITY_POOL_ID: 'us-east-2:af239b6f-bfa8-4c17-92c9-1165825161ea',
+  },
+};
