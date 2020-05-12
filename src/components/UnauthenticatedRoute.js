69c2221c77d471fdc9a1e2105638611304a56b86
Pu Du <rocketsboy@gmail.com>
2020-05-12 16:43:52 -0500

    building our React app

--- src/components/UnauthenticatedRoute.js
+++ src/components/UnauthenticatedRoute.js
@@ -4,7 +4,30 @@ import { useAppContext } from '../libs/contextLib';
 
 export default function UnauthenticatedRoute({ children, ...rest }) {
   const { isAuthenticated } = useAppContext();
+  const redirect = querystring('redirect');
   return (
-    <Route {...rest}>{!isAuthenticated ? children : <Redirect to="/" />}</Route>
+    <Route {...rest}>
+      {!isAuthenticated ? (
+        children
+      ) : (
+        <Redirect to={redirect === '' || redirect === null ? '/' : redirect} />
+      )}
+    </Route>
   );
+}
+
+function querystring(name, url = window.location.href) {
+  name = name.replace(/[[]]/g, '\\$&');
+
+  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)', 'i');
+  const results = regex.exec(url);
+
+  if (!results) {
+    return null;
+  }
+  if (!results[2]) {
+    return '';
+  }
+
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
