b9907f206b9113fe10ffdc0ed8d326aca8d78043
Pu Du <rocketsboy@gmail.com>
2020-05-12 16:19:05 -0500

    call the create API

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -6,6 +6,7 @@ import { onError } from '../libs/errorLib';
 import config from '../config';
 import './NewNote.css';
 import { API } from 'aws-amplify';
+import { s3Upload } from '../libs/awsLib';
 
 export default function NewNote() {
   const file = useRef(null);
