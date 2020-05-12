329fc05e5eecfa36aaad025b6bd7f5ff84e83fea
Pu Du <rocketsboy@gmail.com>
2020-05-12 18:34:54 -0500

    adding a Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
+  STRIPE_KEY: 'pk_test_bwLEEPCmvyYKPTf4qt895D5V00O2PvMFEw',
   s3: {
     REGION: 'us-east-2',
     BUCKET: 'keepernote',
   },
   apiGateway: {
     REGION: 'us-east-2',
-    URL: 'https://98a8t1olqh.execute-api.us-east-2.amazonaws.com/prod',
+    URL: 'https://api.keepernote.com/dev',
   },
   cognito: {
     REGION: 'us-east-2',
@@ -14,5 +14,31 @@ export default {
     APP_CLIENT_ID: '91eod2k3cq7fkh03stbpqhqo7',
     IDENTITY_POOL_ID: 'us-east-2:af239b6f-bfa8-4c17-92c9-1165825161ea',
   },
+};
+
+const prod = {
   STRIPE_KEY: 'pk_test_bwLEEPCmvyYKPTf4qt895D5V00O2PvMFEw',
+  s3: {
+    REGION: 'us-east-2',
+    BUCKET: 'keepernote',
+  },
+  apiGateway: {
+    REGION: 'us-east-2',
+    URL: 'https://api.keepernote.com/prod',
+  },
+  cognito: {
+    REGION: 'us-east-2',
+    USER_POOL_ID: 'us-east-2_8ssnNpHBX',
+    APP_CLIENT_ID: '91eod2k3cq7fkh03stbpqhqo7',
+    IDENTITY_POOL_ID: 'us-east-2:af239b6f-bfa8-4c17-92c9-1165825161ea',
+  },
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod' ? prod : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config,
 };
