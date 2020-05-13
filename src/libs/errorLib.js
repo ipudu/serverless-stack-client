b13833095ea6169e5acce3637a4b79ba0c9eab3d
Pu Du <rocketsboy@gmail.com>
2020-05-12 21:17:02 -0500

    adding error reporting

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,33 @@
+import * as Sentry from '@sentry/browser';
+
+const isLocal = process.env.NODE_ENV === 'development';
+
+export function initSentry() {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.init({
+    dsn:
+      'https://673f02e865634487a266c0739cad1efb@o391812.ingest.sentry.io/5238446',
+  });
+}
+
 export function onError(error) {
+  let errorInfo = {};
   let message = error.toString();
 
   // Auth errors
   if (!(error instanceof Error) && error.message) {
+    errorInfo = error;
     message = error.message;
+    error = new Error(message);
+    // API errors
+  } else if (error.config && error.config.url) {
+    errorInfo.url = error.config.url;
   }
+
+  logError(error, errorInfo);
 
   alert(message);
 }
